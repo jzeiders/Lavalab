74b9167969691b57b5e8446cecc0f8eb669699dd
Will Durkee <wdurkee@usc.edu>
2017-08-03 18:17:59 +0530

    adding NavBar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import YouTube from 'react-youtube';
 
 import Landing from './components/Landing';
 import NewCohort from './components/NewCohort';
+import NavBar from './components/NavBar';
 
 class App extends Component {
   render() {
     return (
       <div className="container">
+        <NavBar />
         <Landing />
         <NewCohort />
       </div>
