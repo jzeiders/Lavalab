23e07ad76f0c8f340f4f97ea500f6f1f9f39d87c
Will Durkee <wdurkee@usc.edu>
2017-08-04 13:05:38 +0530

    removing any bootstrap ish

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
 import '../App.css';
 
 class NavBar extends Component {
   render() {
     return (
 
-    <Navbar inverse collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
-          <a href="#">LavaBabies</a>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-    </Navbar.Header>
-    <Navbar.Collapse>
-      <Nav>
-        <NavItem eventKey={1} href="#">Dolla</NavItem>
-        <NavItem eventKey={2} href="#">Dolla</NavItem>
-      </Nav>
-      <Nav pullRight>
-        <NavItem eventKey={1} href="#">Bill</NavItem>
-        <NavItem eventKey={2} href="#">Yall</NavItem>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+    <div>A frickin nav bar</div>
 
     );
   }
